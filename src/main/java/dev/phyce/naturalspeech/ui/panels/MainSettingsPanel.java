15676c02b0fa6263bdbc27b3516756e5e68a71c2
Louis Hong <honglouis97@gmail.com>
2024-03-17 23:10:45 -0700

    Changed the instruction link to point back into GitHub masters

--- src/main/java/dev/phyce/naturalspeech/ui/panels/MainSettingsPanel.java
+++ src/main/java/dev/phyce/naturalspeech/ui/panels/MainSettingsPanel.java
@@ -188,7 +188,7 @@ public class MainSettingsPanel extends PluginPanel {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				try {
-					Desktop.getDesktop().browse(new URI("https://runelite.net/plugin-hub/show/naturalspeech"));
+					Desktop.getDesktop().browse(new URI("https://github.com/phyce/rl-natural-speech"));
 				} catch (Exception ex) {
 					log.error("Error opening instruction link.", ex);
 				}
