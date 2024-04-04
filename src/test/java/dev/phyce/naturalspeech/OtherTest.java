77a892b237dfd6542de0e4af69582f6ee95ac13e
Louis Hong <honglouis97@gmail.com>
2024-04-04 14:34:15 -0700

    commented out tests

--- src/test/java/dev/phyce/naturalspeech/OtherTest.java
+++ src/test/java/dev/phyce/naturalspeech/OtherTest.java
@@ -47,94 +47,94 @@ public class OtherTest {
 		"This is a Skillcape of Woodcutting, wearing one increases your chance of finding bird's nests. Only a person who has achieved the highest possible level in a skill can wear one.",
 	};
 
-	@Test
-	public void testSplitSentence() {
-		for (String exampleSentence : exampleSentences) {
-			List<String> segments = TextUtil.splitSentence(exampleSentence);
-			String str = segments.stream().map(s -> "[" + s + "]").reduce("", (a, b) ->  a + b);
-			System.out.print(segments.size());
-			System.out.println(str);
-		}
-	}
-
-	@Test
-	public void testSplitSentenceNew() {
-		for (String exampleSentence : exampleSentences) {
-			List<String> segments = TextUtil.splitSentenceV2(exampleSentence);
-			String str = segments.stream().map(s -> "[" + s + "]").reduce("", (a, b) ->  a + b);
-			System.out.print(segments.size());
-			System.out.println(str);
-		}
-	}
-
-	@Test
-	public void testAudioEngine() throws InterruptedException {
-		AudioEngine engine = new AudioEngine();
-		File file = Path.of(System.getProperty("user.home"), "test.wav").toFile();
-		if (!file.exists()) {
-			System.err.println("No test wav file found");
-			return;
-		}
-		long startTime = System.currentTimeMillis();
-
-		var updater = new Thread(() -> {
-			while ((System.currentTimeMillis() - startTime) <= 6000) {
-				engine.update();
-			}
-			System.out.println("Stopped updating");
-		});
-		updater.start();
-
-		for (int i = 0; i < 2; i++) {
-			try (AudioInputStream stream = AudioSystem.getAudioInputStream(file)) {
-				System.out.println("Playing");
-				engine.play("testing", stream, () -> {
-					float t = (System.currentTimeMillis() - startTime);
-					float v = (float) (1 - (t / 6000)) * -20;
-					if (v > 6) v = 6;
-					return v;
-				});
-			} catch (UnsupportedAudioFileException | IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		System.out.println("Joining");
-		updater.join();
-
-	}
-
-	@Test
-	public void testAudio() throws LineUnavailableException {
-		AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
-			22050.0F, // Sample Rate (per second)
-			16, // Sample Size (bits)
-			1, // Channels
-			2, // Frame Size (bytes)
-			22050.0F, // Frame Rate (same as sample rate because PCM is 1 sample per 1 frame)
-			false); // Little Endian
-		var mixer = AudioSystem.getMixer(null);
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-		int maxLine = mixer.getMaxLines(info);
-		int testWidth = 40;
-		long startTime = System.currentTimeMillis();
-		int testCount = 1;
-		for (int i = 0; i < testCount; i++) {
-			DynamicLine line = new DynamicLine((SourceDataLine) mixer.getLine(info));
-			System.out.println(Arrays.toString(line.getControls()));
-			System.out.println(line.isControlSupported(FloatControl.Type.MASTER_GAIN));
-			line.open();
-			System.out.println(Arrays.toString(line.getControls()));
-			System.out.println(line.isControlSupported(FloatControl.Type.MASTER_GAIN));
-//			System.out.print("0");
-//			if (i % testWidth == 0) {
-//				System.out.println();
+//	@Test
+//	public void testSplitSentence() {
+//		for (String exampleSentence : exampleSentences) {
+//			List<String> segments = TextUtil.splitSentence(exampleSentence);
+//			String str = segments.stream().map(s -> "[" + s + "]").reduce("", (a, b) ->  a + b);
+//			System.out.print(segments.size());
+//			System.out.println(str);
+//		}
+//	}
+//
+//	@Test
+//	public void testSplitSentenceNew() {
+//		for (String exampleSentence : exampleSentences) {
+//			List<String> segments = TextUtil.splitSentenceV2(exampleSentence);
+//			String str = segments.stream().map(s -> "[" + s + "]").reduce("", (a, b) ->  a + b);
+//			System.out.print(segments.size());
+//			System.out.println(str);
+//		}
+//	}
+//
+//	@Test
+//	public void testAudioEngine() throws InterruptedException {
+//		AudioEngine engine = new AudioEngine();
+//		File file = Path.of(System.getProperty("user.home"), "test.wav").toFile();
+//		if (!file.exists()) {
+//			System.err.println("No test wav file found");
+//			return;
+//		}
+//		long startTime = System.currentTimeMillis();
+//
+//		var updater = new Thread(() -> {
+//			while ((System.currentTimeMillis() - startTime) <= 6000) {
+//				engine.update();
 //			}
-//			System.out.flush();
-		}
-		long endTime = System.currentTimeMillis();
-
-
-		System.out.println("DONE - open costs: " + ((endTime - startTime)) / testCount  + "ms");
-	}
+//			System.out.println("Stopped updating");
+//		});
+//		updater.start();
+//
+//		for (int i = 0; i < 2; i++) {
+//			try (AudioInputStream stream = AudioSystem.getAudioInputStream(file)) {
+//				System.out.println("Playing");
+//				engine.play("testing", stream, () -> {
+//					float t = (System.currentTimeMillis() - startTime);
+//					float v = (float) (1 - (t / 6000)) * -20;
+//					if (v > 6) v = 6;
+//					return v;
+//				});
+//			} catch (UnsupportedAudioFileException | IOException e) {
+//				throw new RuntimeException(e);
+//			}
+//		}
+//
+//		System.out.println("Joining");
+//		updater.join();
+//
+//	}
+//
+//	@Test
+//	public void testAudio() throws LineUnavailableException {
+//		AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED,
+//			22050.0F, // Sample Rate (per second)
+//			16, // Sample Size (bits)
+//			1, // Channels
+//			2, // Frame Size (bytes)
+//			22050.0F, // Frame Rate (same as sample rate because PCM is 1 sample per 1 frame)
+//			false); // Little Endian
+//		var mixer = AudioSystem.getMixer(null);
+//		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+//		int maxLine = mixer.getMaxLines(info);
+//		int testWidth = 40;
+//		long startTime = System.currentTimeMillis();
+//		int testCount = 1;
+//		for (int i = 0; i < testCount; i++) {
+//			DynamicLine line = new DynamicLine((SourceDataLine) mixer.getLine(info));
+//			System.out.println(Arrays.toString(line.getControls()));
+//			System.out.println(line.isControlSupported(FloatControl.Type.MASTER_GAIN));
+//			line.open();
+//			System.out.println(Arrays.toString(line.getControls()));
+//			System.out.println(line.isControlSupported(FloatControl.Type.MASTER_GAIN));
+////			System.out.print("0");
+////			if (i % testWidth == 0) {
+////				System.out.println();
+////			}
+////			System.out.flush();
+//		}
+//		long endTime = System.currentTimeMillis();
+//
+//
+//		System.out.println("DONE - open costs: " + ((endTime - startTime)) / testCount  + "ms");
+//	}
 }
